3de70d1bf6a448a2e20581a2c0928daac56253ef
LuoXiao <luoxiaoit@163.com>
2018-01-11 15:42:53 +0800

    Initial commit

---
+++ src/app/pages/back-end/back-end.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackEndComponent } from './back-end.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BackEndComponent]
+})
+export class BackEndModule { }
---
+++ src/app/pages/front-end/front-end.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FrontEndComponent } from './front-end.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FrontEndComponent]
+})
+export class FrontEndModule { }
